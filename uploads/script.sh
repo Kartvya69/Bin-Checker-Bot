6534380e0b08925e1b1fbefb75fb1332d46ea2b3
GaaCBot <noreply@gaac.vercel.app>
2024-09-10 22:56:48 +0530

    Uploaded script.sh via GAAC

---
+++ uploads/script.sh
@@ -0,0 +1,7 @@
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+rm -f packages.microsoft.gpg
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update
+sudo apt-get install google-chrome-stable
